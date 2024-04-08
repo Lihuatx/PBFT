1d238a2c0571ae77a98cf8f111920199ae28c919
Lihuatx <1902561088@qq.com>
2024-04-08 11:57:23 +0800

    Add test.sh

--- CreateCluster.py
+++ CreateCluster.py
@@ -2,15 +2,21 @@ import subprocess
 import time
 import sys
 
+# 检查命令行参数的数量
+if len(sys.argv) < 4:
+    print("Usage: python script.py <arg> <nodes_per_group> <nodeNum>")
+    sys.exit(1)
+
+arg = sys.argv[1]
+nodes_per_group = int(sys.argv[2])
+nodeNum = int(sys.argv[3])
+
 # 定义命令模板和数量
 command_template = './app'
 groups = ['N', 'M', 'P']
-nodes_per_group = 19
-arg = sys.argv[2]
-nodes_per_group = int(arg)
 
 # 生成命令列表
-commands = [(command_template, f'{group}{i}', group) for group in groups for i in range(nodes_per_group)]
+commands = [(command_template, f'{group}{i}', group, str(nodeNum)) for group in groups for i in range(nodes_per_group)]
 
 def run_commands(arg):
     print("Starting commands...")
@@ -25,17 +31,17 @@ def run_commands(arg):
 
     subprocess.run(['tmux', 'new-session', '-d', '-s', 'myPBFT'])
 
-
     # 根据提供的 arg 值过滤命令
-    filtered_commands = [(exe, arg1, arg2) for exe, arg1, arg2 in commands if arg2 == arg]
+    filtered_commands = [cmd for cmd in commands if cmd[2] == arg]
 
     # 遍历过滤后的命令列表
-    for index, (exe, arg1, arg2) in enumerate(filtered_commands):
+    for index, (exe, arg1, arg2, nodeNum) in enumerate(filtered_commands):
         window_name = f"app-{arg1}"
         subprocess.run(['tmux', 'new-window', '-t', f'myPBFT:{index + 1}', '-n', window_name])
         time.sleep(0.1)
 
-        tmux_command = f"tmux send-keys -t myPBFT:{index + 1} '{exe} {arg1} {arg2}' C-m"
+        # 在每个命令的末尾添加 nodeNum
+        tmux_command = f"tmux send-keys -t myPBFT:{index + 1} '{exe} {arg1} {arg2} {nodeNum}' C-m"
         subprocess.run(['bash', '-c', tmux_command])
 
     time.sleep(2)
@@ -53,26 +59,22 @@ def run_commands_MP():
 
     subprocess.run(['tmux', 'new-session', '-d', '-s', 'myPBFT'])
 
-
     # 根据提供的 arg 值过滤命令
-    filtered_commands = [(exe, arg1, arg2) for exe, arg1, arg2 in commands if arg2 != "N"]
+    filtered_commands = [cmd for cmd in commands if cmd[2] != "N"]
 
     # 遍历过滤后的命令列表
-    for index, (exe, arg1, arg2) in enumerate(filtered_commands):
+    for index, (exe, arg1, arg2, nodeNum) in enumerate(filtered_commands):
         window_name = f"app-{arg1}"
         subprocess.run(['tmux', 'new-window', '-t', f'myPBFT:{index + 1}', '-n', window_name])
         time.sleep(0.1)
 
-        tmux_command = f"tmux send-keys -t myPBFT:{index + 1} '{exe} {arg1} {arg2}' C-m"
+        # 在每个命令的末尾添加 nodeNum
+        tmux_command = f"tmux send-keys -t myPBFT:{index + 1} '{exe} {arg1} {arg2} {nodeNum}' C-m"
         subprocess.run(['bash', '-c', tmux_command])
 
     time.sleep(2)
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python script.py <arg>")
-        sys.exit(1)
-    arg = sys.argv[1]
     if arg == "N":
         run_commands(arg)
     else:
--- main.go
+++ main.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"os"
+	"simple_pbft/pbft/consensus"
 	"simple_pbft/pbft/network"
+	"strconv"
 )
 
 func main() {
 	genRsaKeys("N")
 	genRsaKeys("M")
 	genRsaKeys("P")
-
 	nodeID := os.Args[1]
 	clusterName := os.Args[2]
+	nodeNumStr := os.Args[3]
+	// 将字符串转换为整数
+	nodeNum, _ := strconv.Atoi(nodeNumStr)
+	consensus.F = nodeNum
 	server := network.NewServer(nodeID, clusterName)
 
 	server.Start()
