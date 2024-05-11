fd51bdb9fb3082a6b6c75b5459299533abf4a8e7
Lihuatx <1902561088@qq.com>
2024-05-11 19:35:50 +0800

    增加5台服务器的测试脚本

--- cmd.py
+++ cmd.py
@@ -7,8 +7,8 @@ import saveData
 
 exeCluster = sys.argv[1]
 cluster_num = sys.argv[3] + " "
-node_numList = ["40 ", "46 "]
-PrimaryClusterWaitTime = 10
+node_numList = ["16 ", "22 ","28 ", "34 "]
+PrimaryClusterWaitTime = 5
 
 # 定义集群中的不同模式以及服务器IP（可以按实际情况填入具体IP地址）
 clusters = ['N', 'M', 'P', 'J', 'K']
