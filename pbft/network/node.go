0cb912643a14dd58798bc662dcda44fa10d67c47
Lihuatx <1902561088@qq.com>
2024-05-15 19:40:04 +0800

    增加5台服务器的测试脚本

--- pbft/network/node.go
+++ pbft/network/node.go
@@ -236,7 +236,7 @@ func (node *Node) Broadcast(cluster string, msg interface{}, path string) map[st
 		}
 		//fmt.Printf("Send to %s Size of JSON message: %d bytes\n", url+path, len(jsonMsg))
 		send(url+path, jsonMsg)
-		//time.Sleep(5 * time.Millisecond)
+		time.Sleep(2 * time.Millisecond)
 
 	}
 
